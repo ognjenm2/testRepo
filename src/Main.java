82448f037633821633101a223df41cf235b19a80
ognjen <ognjen.minic.enterp@gmail.com>
2022-05-13 01:53:43 +0200

    version 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    static int counter = 0;
+    static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    for (int i = 1; i <= 10000; i++) {
+                        Main.counter++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    for (int i = 1; i <= 10000; i++) {
+                        Main.counter++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final value: " + Main.counter);
+    }
+}
